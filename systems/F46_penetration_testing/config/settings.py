c428640a61d77ce52f7fc50511af8b8e57cf26d2
MahsaKeikha <mahsa.keikha@gmail.com>
2026-08-21 01:11:17 -0700

    Add F46 configuration

---
+++ systems/F46_penetration_testing/config/settings.py
@@ -0,0 +1,8 @@
+"""Configuration for F46 Penetration Testing."""
+
+DEFAULTS = {
+    "require_written_authorization": True,
+    "allow_destructive_actions": False,
+    "require_human_approval": True,
+    "retain_evidence": True,
+}
