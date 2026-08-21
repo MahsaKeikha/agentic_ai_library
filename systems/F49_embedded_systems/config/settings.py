03bd10508eaead5bf727fb63a9230bb04f5dcef3
MahsaKeikha <mahsa.keikha@gmail.com>
2026-08-21 01:14:30 -0700

    Add F49 configuration

---
+++ systems/F49_embedded_systems/config/settings.py
@@ -0,0 +1,8 @@
+"""Configuration for F49 Embedded Systems."""
+
+DEFAULTS = {
+    "require_requirement_traceability": True,
+    "require_interface_review": True,
+    "require_verification_evidence": True,
+    "require_human_release_approval": True,
+}
