54bbafb0c3e1201f24b7df7e84645837f2e0cdf0
Mahsa Keikha <mahsa.keikha@gmail.com>
2026-08-23 14:52:09 -0700

    Use canonical Google Appointment Schedule URL

--- docs/lead-config.js
+++ docs/lead-config.js
@@ -3,5 +3,5 @@
 // FormSubmit is used only for high-level assessment inquiries. Detailed evidence must move to an approved private channel.
 
 window.AGENTIC_LEAD_ENDPOINT = "https://formsubmit.co/ajax/mahsa@connectioncare.net";
-window.AGENTIC_SCHEDULING_URL = "https://calendar.app.google/fxVJM1GEa28aqgWq6";
-window.AGENTIC_SCHEDULING_FALLBACK_URL = "https://calendar.app.google/2vRv6fp4cpsfDw6q8";
+window.AGENTIC_SCHEDULING_URL = "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0acPNJ71vLUnFG0ThVy4Z30aAPQIXfsMGiT8Gb2wtqBfO2QEtaSOZwn_y8wmPTuGyNm8OfAPTF";
+window.AGENTIC_SCHEDULING_FALLBACK_URL = "https://calendar.app.google/fxVJM1GEa28aqgWq6";
